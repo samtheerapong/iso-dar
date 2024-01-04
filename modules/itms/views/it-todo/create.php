fcf99470acfd945bde52696f5e78119aa626e3fa
samtheerapong <theerapong.khan@gmail.com>
2024-01-04 22:42:46 +0700

    up

--- modules/itms/views/it-todo/create.php
+++ modules/itms/views/it-todo/create.php
@@ -15,6 +15,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $this->render('_form', [
         'model' => $model,
+        'initialPreview'=>[],
+        'initialPreviewConfig'=>[]
     ]) ?>
 
 </div>
--- modules/itms/views/it-todo/update.php
+++ modules/itms/views/it-todo/update.php
@@ -18,6 +18,8 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 
     <?= $this->render('_form', [
         'model' => $model,
+        'initialPreview'=>[],
+        'initialPreviewConfig'=>[]
     ]) ?>
 
 </div>
