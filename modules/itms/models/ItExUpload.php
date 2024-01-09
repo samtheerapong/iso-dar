fc06f945fa42b70bdf9d89131f91c1879bddcdc6
samtheerapong <theerapong.khan@gmail.com>
2024-01-09 16:44:19 +0700

    u

--- modules/itms/models/ItExUpload.php
+++ modules/itms/models/ItExUpload.php
@@ -4,6 +4,7 @@ namespace app\modules\itms\models;
 
 use Yii;
 use yii\helpers\Html;
+use yii\helpers\Json;
 use yii\helpers\Url;
 
 /**
@@ -15,8 +16,9 @@ use yii\helpers\Url;
  */
 class ItExUpload extends \yii\db\ActiveRecord
 {
+    const UPLOAD_FOLDER = 'uploads/ex/doc';
+
     const UPLOAD_FOLDER_IMG = 'uploads/ex/img';
-    const UPLOAD_FOLDER_DOC = 'uploads/ex/doc';
     /**
      * {@inheritdoc}
      */
@@ -31,9 +33,8 @@ class ItExUpload extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['img_ref', 'title'], 'string', 'max' => 255],
+            [['img_ref', 'title', 'doc_ref', 'docs'], 'string', 'max' => 255],
             [['img_ref'], 'unique'],
-            [['doc_ref'], 'unique']
         ];
     }
 
@@ -45,6 +46,7 @@ class ItExUpload extends \yii\db\ActiveRecord
         return [
             'id' => Yii::t('app', 'ID'),
             'title' => Yii::t('app', 'Title'),
+            'docs' => Yii::t('app', 'Documents'),
             'img_ref' => Yii::t('app', 'img_ref'),
             'doc_ref' => Yii::t('app', 'doc_ref'),
         ];
@@ -88,31 +90,78 @@ class ItExUpload extends \yii\db\ActiveRecord
         }
     }
 
-    // uploading doc
-    public static function getUploadPathDoc()
+    // uploading docs
+    public static function getUploadPath()
     {
-        return Yii::getAlias('@webroot') . '/' . self::UPLOAD_FOLDER_DOC . '/';
+        return Yii::getAlias('@webroot') . '/' . self::UPLOAD_FOLDER . '/';
     }
 
-    public static function getUploadUrlDoc()
+    public static function getUploadUrl()
     {
-        return Url::base(true) . '/' . self::UPLOAD_FOLDER_DOC . '/';
+        return Url::base(true) . '/' . self::UPLOAD_FOLDER . '/';
     }
 
-    // Add this method to your ItExUpload model class
-    public function getDocumentList()
+    //********** List Downloads */
+    public function listDownloadFiles($type)
     {
-        $docs = UploadDoc::find()->where(['ref' => $this->doc_ref])->all();
-
-        $list = [];
-        foreach ($docs as $doc) {
-            $filePath = Yii::$app->urlManager->createAbsoluteUrl([$doc->file_path]);
-            $list[] = [
-                'name' => $doc->file_name,
-                'url' => $filePath,
-            ];
+        $docs_file = '';
+        if (in_array($type, ['docs'])) {
+            $data = $type === 'docs' ? $this->docs : '';
+            $files = Json::decode($data);
+            if (is_array($files)) {
+                $docs_file = '<ul>';
+                foreach ($files as $key => $value) {
+                    if (strpos($value, '.jpg') !== false || strpos($value, '.jpeg') !== false || strpos($value, '.png') !== false || strpos($value, '.gif') !== false) {
+                        $thumbnail = Html::img(['/documents/download', 'id' => $this->id, 'file' => $key, 'fullname' => $value], ['class' => 'img-thumbnail', 'alt' => 'Image', 'style' => 'width: 150px']);
+                        $fullSize = Html::a($thumbnail, ['/documents/download', 'id' => $this->id, 'file' => $key, 'fullname' => $value], ['target' => '_blank']);
+                        $docs_file .= '<li>' . $fullSize . '</li>';
+                    } else {
+                        $docs_file .= '<li>' . Html::a($value, ['/documents/download', 'id' => $this->id, 'file' => $key, 'fullname' => $value]) . '</li>';
+                    }
+                }
+                $docs_file .= '</ul>';
+            }
         }
 
-        return $list;
+        return $docs_file;
+    }
+
+    //********** initialPreview */    
+    public function isImage($filePath)
+    {
+        return @is_array(getimagesize($filePath)) ? true : false;
+    }
+
+    public function initialPreview($data, $field, $type = 'file')
+    {
+        $initial = [];
+        $files = Json::decode($data);
+        if (is_array($files)) {
+            foreach ($files as $key => $value) {
+                $filePath = self::getUploadUrl() . $this->ref . '/' . $value;
+                $filePathDownload = self::getUploadUrl() . $this->ref . '/' . $value;
+
+                $isImage = $this->isImage($filePath);
+
+                if ($type == 'file') {
+                    $initial[] = "<div class='file-preview-other'><h2><i class='glyphicon glyphicon-file'></i></h2></div>";
+                } elseif ($type == 'config') {
+                    $initial[] = [
+                        'caption' => $value,
+                        'width'  => '120px',
+                        'url'    => Url::to(['/itms/it-ex-upload/deletefile', 'id' => $this->id, 'fileName' => $key, 'field' => $field]),
+                        'key'    => $key
+                    ];
+                } else {
+                    if ($isImage) {
+                        $file = Html::img($filePath, ['class' => 'file-preview-image', 'alt' => $this->file_name, 'title' => $this->file_name]);
+                    } else {
+                        $file = Html::a('View File', $filePathDownload, ['target' => '_blank']);
+                    }
+                    $initial[] = $file;
+                }
+            }
+        }
+        return $initial;
     }
 }
